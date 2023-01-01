36f8db867e949d010bc792e834ef76d41075b463
MattiaRiola <s280169@studenti.polito.it>
2023-01-01 20:56:04 +0100

    🎨🚧 experience timeline

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import "react-vertical-timeline-component/style.min.css"
 
 function Experience() {
   return (
-    <div>Experience</div>
+    <div className='experience'>
+      <VerticalTimeline >
+        <VerticalTimelineElement>
+          <h3 className="vertical-timeline-element-title">Software Engineer</h3>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+
+    </div>
   )
 }
 
