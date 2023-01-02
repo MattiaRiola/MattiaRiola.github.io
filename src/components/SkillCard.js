448d55c249d8e9cf401cf2da18957162ee9f53ba
MattiaRiola <s280169@studenti.polito.it>
2023-01-02 18:47:55 +0100

    🎨colors

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from 'react-bootstrap/Card';
 function SkillCard(props) {
   return (
-    <Card  className="shadow-lg p-3 mb-3 bg-dark rounded">
+    <Card  className="shadow-lg p-3 mb-3 bg-secondary rounded">
     <Card.Body>
       <Card.Title>{props.title}</Card.Title>
       <Card.Text>
---
+++ src/styles/Colors.js
@@ -0,0 +1,17 @@
+
+const colors = {
+    primary:        '#90caf9',
+    primaryLight:   '#e6ffff',
+    primaryDark:    '#82b3c9',
+    secondary:      '#c5cae9',
+    secondaryLight: '#f8fdff',
+    secondaryDark:  '#9499b7',
+    success:        '#198754',
+    info:           '#4b475a',
+    warning:        '#ddab65',
+    danger:         '#e98a83',
+    light:          '#e1eff7',
+    dark:           '#93a3ac',
+}
+
+export default colors;
