9ea35f116d914cc3d2dcd73653921dd3be60117d
MattiaRiola <s280169@studenti.polito.it>
2023-01-05 22:53:44 +0100

    🍱#2 add animated icons and update credits close #2

--- src/pages/Credits.js
+++ src/pages/Credits.js
@@ -2,11 +2,87 @@ import React from 'react'
 
 
 const credits = [
-    {
-        icon: "coding",
-        website: "https://www.flaticon.com/free-animated-icons/coding",
-        description: "Coding animated icons created by Freepik - Flaticon"
-    }
+  {
+    icon: "arcade",
+    website: "https://www.flaticon.com/free-animated-icons/arcade",
+    description: "Arcade animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "briefcase",
+    website: "https://www.flaticon.com/free-animated-icons/briefcase",
+    description: "Briefcase animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "book",
+    website: "https://www.flaticon.com/free-animated-icons/book",
+    description: "Book animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "chip",
+    website: "https://www.flaticon.com/free-animated-icons/chip",
+    description: "Chip animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "coding",
+    website: "https://www.flaticon.com/free-animated-icons/coding",
+    description: "Coding animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "diploma",
+    website: "https://www.flaticon.com/free-animated-icons/diploma",
+    description: "Diploma animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "eco-battery",
+    website: "https://www.flaticon.com/free-animated-icons/eco-battery",
+    description: "Eco battery animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "open-book",
+    website: "https://www.flaticon.com/free-animated-icons/open-book",
+    description: "Open book animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "school",
+    website: "https://www.flaticon.com/free-animated-icons/school",
+    description: "School animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "server",
+    website: "https://www.flaticon.com/free-animated-icons/server",
+    description: "Server animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "smartphone",
+    website: "https://www.flaticon.com/free-animated-icons/smartphone",
+    description: "Smartphone animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "solar-panel",
+    website: "https://www.flaticon.com/free-animated-icons/solar-panel",
+    description: "Solar panel animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "university",
+    website: "https://www.flaticon.com/free-animated-icons/university",
+    description: "University animated icons created by Freepik - Flaticon"
+
+  },
+  {
+    icon: "web-page",
+    website: "https://www.flaticon.com/free-animated-icons/web-page",
+    description: "Web page animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "workplace",
+    website: "https://www.flaticon.com/free-animated-icons/workplace",
+    description: "Workplace animated icons created by Freepik - Flaticon"
+  },
+  {
+    icon: "work-list",
+    website: "https://www.flaticon.com/free-animated-icons/work-list",
+    description: "Work list animated icons created by Freepik - Flaticon"
+  },
 ]
 
 function Credits() {
