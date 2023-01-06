3bd8044395c0bbe6673e654c81c9eafbe8c900e7
MattiaRiola <s280169@studenti.polito.it>
2023-01-06 02:47:39 +0100

    💄♻️ resize and clean footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import { LinkedIn, Email, GitHub } from '@material-ui/icons'
-import { Nav, Navbar, NavItem} from 'react-bootstrap'
-import {Link }from 'react-router-dom';
+import { Nav, Navbar, NavItem } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+
 
 function Footer() {
     return (
-        <Navbar  bg="dark"variant="dark" sticky="bottom">
-                <Nav className="m-auto">
-                    <NavItem>
-                        <div className="footer">
-                            <div>
-                                <LinkedIn />
-                                <Email />
-                                <GitHub />
-                                <text>   </text>
-                                <Link  style={{color: 'black'}} to={"/credits"} >  credits</Link>
-                                <text> &copy; 2022 mattiariola.github.com</text>
-                            </div>
-                        </div>
-                    </NavItem>
-                </Nav>
+        <Navbar bg="dark" variant="dark" sticky="bottom">
+            <Nav className="m-auto">
+                <NavItem>
+                    <LinkedIn />
+                    <Email />
+                    <GitHub />
+                </NavItem>
+                <NavItem className='ms-3'>
+                    <Link  style={{ color: 'black' }} to={"/credits"} >  credits</Link>
+                </NavItem>
+                <NavItem className='ms-3'>
+                    <small> &copy; 2022 mattiariola.github.com</small>
+                </NavItem>
+            </Nav>
         </Navbar>
     )
 }
