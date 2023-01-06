7cf574a887cb6db6c9102b155a831ac5332fa568
MattiaRiola <s280169@studenti.polito.it>
2023-01-06 15:34:48 +0100

    💄 close #7 use icons in navbar and beautify text

--- src/components/PortfolioNavbar.js
+++ src/components/PortfolioNavbar.js
@@ -2,17 +2,19 @@ import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Link }from 'react-router-dom';
-
+import Home from '@material-ui/icons/Home';
 function PortfolioNavbar() {
   return (
     <>
         <Navbar bg="primary" variant="primary">
           
-            <Navbar.Brand className="ms-2" href="home">Navbar</Navbar.Brand>
+            <Navbar.Brand className="ms-2" href="home">
+            <Home/>
+            </Navbar.Brand>
             <Nav className="ms-2">
-              <Nav.Link as={Link} to="/home">Home</Nav.Link>
-              <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-              <Nav.Link as={Link} to="/experience">Experience</Nav.Link>
+              <Nav.Link as={Link} style={{fontSize: 15}} to="/home">Home</Nav.Link>
+              <Nav.Link as={Link} style={{fontSize: 15}} to="/projects">Projects</Nav.Link>
+              <Nav.Link as={Link} style={{fontSize: 15}} to="/experience">Experience</Nav.Link>
             </Nav>
           </Navbar>
         <br />
