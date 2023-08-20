88390185f0d87f319b761220623b8e107f740777
MattiaRiola <s280169@studenti.polito.it>
2023-08-20 18:46:50 +0200

    🪲 fix navbar links, align it to the center and resize

--- src/components/PortfolioNavbar.js
+++ src/components/PortfolioNavbar.js
@@ -2,19 +2,15 @@ import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Link }from 'react-router-dom';
-import Home from '@material-ui/icons/Home';
+
 function PortfolioNavbar() {
   return (
     <>
-        <Navbar bg="primary" variant="primary">
-          
-            <Navbar.Brand className="ms-2" href="home">
-            <Home/>
-            </Navbar.Brand>
+        <Navbar bg="primary" variant="primary" className="justify-content-center">
             <Nav className="ms-2">
-              <Nav.Link as={Link} style={{fontSize: 15}} to="/home">Home</Nav.Link>
-              <Nav.Link as={Link} style={{fontSize: 15}} to="/projects">Projects</Nav.Link>
-              <Nav.Link as={Link} style={{fontSize: 15}} to="/experience">Experience</Nav.Link>
+              <Nav.Link as={Link} style={{fontSize: 20}} to="/home">Home</Nav.Link>
+              <Nav.Link as={Link} style={{fontSize: 20}} to="/projects">Projects</Nav.Link>
+              <Nav.Link as={Link} style={{fontSize: 20}} to="/experience">Experience</Nav.Link>
             </Nav>
           </Navbar>
         <br />
