e4327ddc4c907a943ad775175b91614f3b1ebb47
MattiaRiola <s280169@studenti.polito.it>
2023-08-24 00:53:05 +0200

    💄 improve project card aesthetic

--- src/components/cards/ProjectCard.js
+++ src/components/cards/ProjectCard.js
@@ -10,65 +10,53 @@ import colors from '../../styles/Colors';
 function ProjectCard(props) {
     const project = props.project;
     const languages = props.project.languages;
-
+  
     return (
-        <Card className="shadow-lg p-3 mb-3 bg-secondary rounded" key={project.title}>
-            <Card.Header>
-                <h2>{project.title}</h2>
-            </Card.Header>
-            <Card.Body>
-                <Container>
-                    <Row xs={1} className="mx-auto">
-                        <Col className="my-auto mx-auto d-block d-lg-none">
-                            <Card.Img 
-                                variant="top"
-                                src={process.env.PUBLIC_URL + '/resources/projects/' + project.img}
-                            />
-                        </Col>
-                    </Row>
-                    <Row className="my-2">
-                        <Col sm={4} md={4} className="my-auto d-none d-lg-block">
-                            <Card.Img 
-                                variant="top"
-                                src={process.env.PUBLIC_URL + '/resources/projects/' + project.img}
-                            />
-                        </Col>
-                        <Col sm={8} md={8} className="my-auto">
-                            <br />
-                            <br />
-                            <br />
-                            <Card.Text>
-                                <ul>
-                                    <li>
-                                        {"📄 "+ project.description}
-                                    </li>
-                                    <li>
-                                        {"🎯 "+project.objectives}
-                                    </li>
-                                    <li>
-                                        {getPurposeEmoji(project.purpose) + " " + project.purpose}
-                                    </li>
-                                    <li>
-                                        {"👤".repeat(project.teamSize)}
-                                    </li>
-                                </ul>
-                            </Card.Text>
-                            <br />
-                            <br />
-                            {project.url ? 
-                                <a style={{ color: colors.grey }} href={project.url} title={project.title}>
-                                    more information
-                                </a> : <></>
-                            }
-                            <br />
-                            <br />
-                            <LanguageChips languages={languages} />
-                            
-                        </Col>
-                    </Row>
-                </Container>
-            </Card.Body>
-        </Card>
+      <Card className="shadow-lg p-3 mb-3 bg-secondary rounded" key={project.title}>
+          <Card.Header>
+              <h2>{project.title}</h2>
+          </Card.Header>
+          <Card.Body>
+              <Container>
+                  <Row xs={1} className="mx-auto">
+                      <Col className="my-auto mx-auto d-block d-lg-none">
+                          <Card.Img 
+                              variant="top"
+                              src={process.env.PUBLIC_URL + '/resources/projects/' + project.img}
+                          />
+                      </Col>
+                  </Row>
+                  <Row className="my-2">
+                      <Col sm={3} md={3} className="my-auto d-none d-lg-block">
+                          <Card.Img 
+                              variant="top"
+                              src={process.env.PUBLIC_URL + '/resources/projects/' + project.img}
+                          />
+                      </Col>
+                      <Col sm={9} md={9} className="my-auto">
+                          <div>
+                              <br />
+                              <p>📄 {project.description}</p>
+                              <p>🎯 {project.objectives}</p>
+                              <br />
+                          </div>
+                          {project.url ? 
+                              <a style={{ color: colors.grey }} href={project.url} title={project.title}>
+                                  more information
+                              </a> : <></>
+                          }
+                          <br />
+                          <br />
+                          <LanguageChips languages={languages} />
+                      </Col>
+                  </Row>
+              </Container>
+          </Card.Body>
+          <Card.Footer className="d-flex justify-content-between">
+              <span>{getPurposeEmoji(project.purpose) + " " + project.purpose}</span>
+              <span>{"team size: "+"👤".repeat(project.teamSize)}</span>
+          </Card.Footer>
+      </Card>
     )
 }
 
@@ -78,6 +66,8 @@ function getPurposeEmoji(purpose) {
         return '🎓';
       case 'personal':
         return '❤️';
+      case 'hobby':
+        return '🎳';
       case 'job':
         return '💼';
       default:
