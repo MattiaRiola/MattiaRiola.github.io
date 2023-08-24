4ee449cb899c3b721fa326d96689308682a1b6be
MattiaRiola <s280169@studenti.polito.it>
2023-08-24 02:19:38 +0200

    💬 grammar and typo fixes

--- src/data/SkillData.js
+++ src/data/SkillData.js
@@ -1,65 +1,60 @@
-
 const skills = [
-    {
-      title: "Build Video Games",
-      description: "I have experience in developing Games as Game Designer, Level Designer and Game programmer using Unity",
-      icon: "arcade.gif",
-      languages: ["C#", "Unity", "Game Design", "Level Design", "Game Programming"],
-      projects: [
-        {title: "But They Are Cats",url:"https://thefellowshipofthebox.itch.io/but-they-are-cats"},
-        //{title: "Space Shooter",url:"https://github.com/MattiaRiola/Space-Shooter-Pro"},
-      ],
-    },
-    {
-      title: "Write Backend Systems",
-      description: "I have experience in developing monolithic server and microservices using Java/Kotlin + Spring Framework and Golang",
-      icon: "server.gif",
-      languages: ["Java", "Kotlin", "Spring", "Golang","C++","NodeJs Express","SQL","MongoDB","Kafka","Maven","Gradle"],
-      projects: [
-        {title: "Public transportation manager",url:"https://github.com/A-short-name/Public-Transportation-Manager"},
-        {title: "SPG",url:"https://github.com/SE2-GROUP-P12/SPG"},
-        {title: "Office queue manager",url: "https://github.com/SE2-GROUP-P12/office-queue-management"},
-        {title: "ezgas",url:"https://github.com/MattiaRiola/ezgas"},
-        {title: "Remote backup system",url:"https://github.com/MattiaRiola/PDS-M1-Project"},
-      ]
-    },
-    {
-      title: "Create User Interfaces",
-      description: "I have experience in developing web applications using React and Node.js; I also have experience in developing android mobile applciation using kotlin and firebase",
-      icon: "web-page.gif",
-      languages: ["React", "Javascript", "React-Bootstrap","Material-UI","HTML","Cypress"],
-      projects: [
-        {title: "Todo manager",url:"https://github.com/MattiaRiola/wa1_todo_dynamic"},
-        {title:"exam survey",url:"https://github.com/MattiaRiola/WA1_exam_survey"},
-      ]
-    },
-    {
-      title: "Develop Mobile Apps",
-      description: "I have experience in developing android mobile applications using kotlin and firebase",
-      icon: "smartphone.gif",
-      languages: ["Kotlin", "Firebase","jetpack compose","android studio"],
-      projects: [
-        {title: "Piggy Timebank",url:"https://github.com/A-short-name/G01-Timebanking"},
-      ]
-    },
-    {
-      title: "Automate Systems",
-      description: "I have experience in developing CI/CD pipelines using Gitlab CI, Docker and Docker-compose",
-      languages: ["Gitlab CI", "Github Workflow","Docker", "Docker-compose", "Sonarcloud", "Shell Scripting", "Bash", "Firebase"],
-      icon: "loop.gif",
-      projects: [
-  
-      ]
-    },
-    {
-      title: "Manage Teams & Tasks",
-      description: "I have experience in managing tasks and teams understanding the importance of communication and organization",
-      languages: ["Waterfall", "Agile", "Scrum Master", "Youtrack", "Trello", "Github", "Gitlab", "Notion", "Draw.io", "Google suite", "Microsoft office"],
-      icon: "sticky-notes.gif",
-      projects: [
-  
-      ]
-    }
-  ];
+  {
+    title: "Build Video Games",
+    description: "I have experience in developing games as a Game Designer, Level Designer, and Game Programmer using Unity",
+    icon: "arcade.gif",
+    languages: ["C#", "Unity", "Game Design", "Level Design", "Game Programming"],
+    projects: [
+      { title: "But They Are Cats", url: "https://thefellowshipofthebox.itch.io/but-they-are-cats" },
+      //{title: "Space Shooter", url: "https://github.com/MattiaRiola/Space-Shooter-Pro"},
+    ],
+  },
+  {
+    title: "Write Backend Systems",
+    description: "I have experience in developing monolithic servers and microservices using Java/Kotlin + Spring Framework and Golang",
+    icon: "server.gif",
+    languages: ["Java", "Kotlin", "Spring", "Golang", "C++", "NodeJs Express", "SQL", "MongoDB", "Kafka", "Maven", "Gradle"],
+    projects: [
+      { title: "Public Transportation Manager", url: "https://github.com/A-short-name/Public-Transportation-Manager" },
+      { title: "SPG", url: "https://github.com/SE2-GROUP-P12/SPG" },
+      { title: "Office Queue Manager", url: "https://github.com/SE2-GROUP-P12/office-queue-management" },
+      { title: "ezgas", url: "https://github.com/MattiaRiola/ezgas" },
+      { title: "Remote Backup System", url: "https://github.com/MattiaRiola/PDS-M1-Project" },
+    ]
+  },
+  {
+    title: "Create User Interfaces",
+    description: "I have experience in developing web applications using React and Node.js; I also have experience in developing Android mobile applications using Kotlin and Firebase",
+    icon: "web-page.gif",
+    languages: ["React", "Javascript", "React-Bootstrap", "Material-UI", "HTML", "Cypress"],
+    projects: [
+      { title: "Todo Manager", url: "https://github.com/MattiaRiola/wa1_todo_dynamic" },
+      { title: "Exam Survey", url: "https://github.com/MattiaRiola/WA1_exam_survey" },
+    ]
+  },
+  {
+    title: "Develop Mobile Apps",
+    description: "I have experience in developing Android mobile applications using Kotlin and Firebase",
+    icon: "smartphone.gif",
+    languages: ["Kotlin", "Firebase", "Jetpack Compose", "Android Studio"],
+    projects: [
+      { title: "Piggy Timebank", url: "https://github.com/A-short-name/G01-Timebanking" },
+    ]
+  },
+  {
+    title: "Automate Systems",
+    description: "I have experience in developing CI/CD pipelines using Gitlab CI, Docker, and Docker-compose",
+    languages: ["Gitlab CI", "Github Workflow", "Docker", "Docker-compose", "Sonarcloud", "Shell Scripting", "Bash", "Firebase"],
+    icon: "loop.gif",
+    projects: []
+  },
+  {
+    title: "Manage Teams & Tasks",
+    description: "I have experience in managing tasks and teams, understanding the importance of communication and organization",
+    languages: ["Waterfall", "Agile", "Scrum Master", "Youtrack", "Trello", "Github", "Gitlab", "Notion", "Draw.io", "Google Suite", "Microsoft Office"],
+    icon: "sticky-notes.gif",
+    projects: []
+  }
+];
 
-  export default skills;
+export default skills;
